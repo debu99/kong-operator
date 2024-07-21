0539176e701f6b2483d132b26ceffac6786d0729
Vincent Liu <vliu@kpler.com>
2024-07-21 21:02:41 +0800

    fix logger

--- internal/controller/kongservice_controller.go
+++ internal/controller/kongservice_controller.go
@@ -52,7 +52,7 @@ type KongServiceReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *KongServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	logger = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
 	// Fetch the KongService instance
 	var kongService kongv1.KongService
